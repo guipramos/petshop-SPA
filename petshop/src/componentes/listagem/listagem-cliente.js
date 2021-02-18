09a0dd192d5ca2eb303d0d7bdec86461f15729b0
Guilherme <gui18ramos@hotmail.com>
2021-02-17 23:26:37 -0300

    Ajustando webpack e instalando modulos

--- petshop/src/componentes/listagem/listagem-cliente.js
+++ petshop/src/componentes/listagem/listagem-cliente.js
@@ -1,8 +1,8 @@
-
+import { deletaCliente, listarClientes} from '../../api/cliente.js'
+import "../../assets/css/clientes.css"
 
 const removeCliente = (id) => {
   if(confirm("Deseja deletar o cliente ?")){
-    debugger;
     deletaCliente(id)
     window.location.reload()
   }
