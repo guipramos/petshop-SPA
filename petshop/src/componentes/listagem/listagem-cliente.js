e2d23e7894627c3bb656ad6d2e06b42fcda8ca11
Guilherme <guilherme.ramos@degraupublicidade.com.br>
2021-02-18 21:22:52 -0300

    Concluindo projeto

--- petshop/src/componentes/listagem/listagem-cliente.js
+++ petshop/src/componentes/listagem/listagem-cliente.js
@@ -8,33 +8,78 @@ const removeCliente = (id) => {
   }
 }
 
-const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+const criarBotaoExcluir = (id) => {
+  const botao = document.createElement('button')
+  botao.classList.add('btn', 'btn-danger')
+  botao.innerHTML = 'Excluir'
 
-const exibeCliente = (cpf, nome, id) => {
+  botao.addEventListener('click', ()=> { 
+    removeCliente(id)
+  })
+  return botao
+}
+
+const criaCorpoTabela = ( tabela ) =>  { 
+
+  const corpoTabela = document.createElement('tbody')
+
+  const exibeCliente = (cpf, nome, id) => {
     const linha = document.createElement('tr');
 
     const conteudoLinha = `
     <td>${cpf}</td>
     <td>${nome}</td>
-    <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
-    <a href="./componentes/edita/edita-clientes.html?id=${id}">
-    <button type=""button class="btn btn-info">Editar</button>
-    </a>
-    
-    
-`
   
+    <button type="button" class="btn btn-info" onclick="navegacao('/edita?id=${id}'); return false;">Editar</button>
+   
+`
     linha.innerHTML = conteudoLinha;
+    linha.appendChild(criarBotaoExcluir(id))
     return linha;
   };
-  
-  listarClientes().then( exibe => {
-  exibe.forEach(indice => {
-    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
-  })
- }
 
- )
+  listarClientes().then( exibe => {
+    exibe.forEach(indice => {
+      corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
+    })
+   }
+  
+   )
+   tabela.appendChild(corpoTabela)
+
+}
+
+const inicializaTabela = () => { 
+
+  const cabecalho = `
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">CPF</th>
+      <th scope="col">Nome</th>
+      <th scope="col"></th>
+      <th scope="col"><a class="btn btn-primary" onclick="navegacao('/cadastro'); return false;">Novo Cliente</a>
+      </th>
+    </tr>
+  </thead>
+`
+const tabela = document.createElement("table")
+tabela.innerHTML = cabecalho
+tabela.classList.add("table")
+
+criaCorpoTabela(tabela)
+
+return tabela
+}
+
+
+export default inicializaTabela
+
+
+
+
+
+  
+  
   
   
  
